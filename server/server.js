f8625366e5d8404f1511c47dae56cf4a62697a3f
bbobylord <erfan.pirikhah1@gmail.com>
2020-01-08 16:44:22 +0330

    first Api

--- server/server.js
+++ server/server.js
@@ -1,16 +1,31 @@
 process.env.NODE_CONFIG_DIR = __dirname + '/config';
 const {User}=require('./model/user')
+
 const config=require('config');
+const _=require('lodash');
+const express=require('express');
+
+
 
 console.log(`*** ${String(config.get('LEVEL')).toUpperCase()}***`);
 
+const app=express();
+app.use(express.json());
 
-let newUser=new User({
-    fullname:'Erfan pirikhah',
-    email:'erfan.com',
-    password:'123456'
+app.post('/api/user',(req,res)=>{
+    const body=_.pick(req.body,['fullname','email','password']);
+
+    
+    let user= new User(body);
+
+    user.save().then(
+        (user)=>{res.status(200).send(user);
+    },  (err) =>{res.status(400).send(`Somthing wrong`)}
+    
+    )
 })
 
-newUser.save().then((User)=>{
-    console.log(User)
+
+app.listen(config.get('PORT'),()=>{
+    console.log(`The Server runnig on Port: ${config.get('PORT')}`);
 })
